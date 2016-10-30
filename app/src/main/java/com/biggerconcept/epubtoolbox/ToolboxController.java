0dfe680904b2ade2c01aadfcf71d3f28431ee769
Andrew Bigger <andrew.bigger@gmail.com>
2016-10-30 11:40:50 +1100

    Attach form to controller actions

--- app/src/main/java/com/biggerconcept/epubtoolbox/ToolboxController.java
+++ app/src/main/java/com/biggerconcept/epubtoolbox/ToolboxController.java
@@ -1,25 +1,90 @@
 package com.biggerconcept.epubtoolbox;
 
+import java.awt.event.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.TreeView;
 
 public class ToolboxController implements Initializable {
     
-    @FXML
-    private Label label;
+    @Override
+    public void initialize(URL url, ResourceBundle rb) { }    
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    public TreeView<String> consoleView;
+    
+    @FXML
+    private void handleUnpackClick(MouseEvent event) {
+        try { System.out.println("Unpack"); }
+        catch (Exception e) {
+            
+        }
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    @FXML
+    private void handlePackClick(MouseEvent event) {
+        try { }
+        catch (Exception e) {
+            
+        }
+    }
+    
+    @FXML
+    private void handlePickClick(MouseEvent event) {
+        try { }
+        catch (Exception e) {
+            
+        }
+    }
+    
+    @FXML
+    private void handleEpubCheckClick(MouseEvent event) {
+        try { }
+        catch (Exception e) {
+            
+        }
+    }
+    
+    @FXML
+    private void handleImageCheckClick(MouseEvent event) {
+        try { }
+        catch (Exception e) {
+            
+        }
+    }
+    
+    @FXML
+    private void handleSizeCheckClick(MouseEvent event) {
+        try { }
+        catch (Exception e) {
+            
+        }
+    }
+    
+    @FXML
+    private void handleAllCheckClick(MouseEvent event) {
+        try { }
+        catch (Exception e) {
+            
+        }
+    }
+    
+    @FXML
+    private void handleArtefactRemove(ActionEvent event) {
+        try { }
+        catch (Exception e) {
+            
+        }
+    }
+    
+    @FXML
+    private void handleItunesMetaRemove(ActionEvent event) {
+        try { }
+        catch (Exception e) {
+            
+        }
+    }
 }
